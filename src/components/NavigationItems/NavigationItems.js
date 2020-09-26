d6fbdf0416297471d8d00d1024d520a16e9c3870
jumper2210 <47790273+jumper2210@users.noreply.github.com>
2020-09-26 19:31:47 +0200

    mobile ver styles: improved

--- src/components/NavigationItems/NavigationItems.js
+++ src/components/NavigationItems/NavigationItems.js
@@ -1,7 +1,7 @@
 import React from "react";
 import classes from "./NavigationItems.module.css";
 import NavigationItem from "../NavigationItems/NavigationItem/NavigationItem";
-const navigationItems = props => (
+const navigationItems = (props) => (
   <ul className={classes.NavigationItems}>
     <NavigationItem link="/">Twoja kanapka</NavigationItem>
     {props.isAuthenticated && !props.isAdmin ? (
@@ -10,11 +10,11 @@ const navigationItems = props => (
     {props.isAdmin && props.isAuthenticated ? (
       <NavigationItem link="/adminView">Panel administratora</NavigationItem>
     ) : null}
-    {!props.isAuthenticated ? (
+    {/* {!props.isAuthenticated ? (
       <NavigationItem link="/auth">Zajerestruj sie!</NavigationItem>
     ) : (
       <NavigationItem link="/logout">Wyloguj</NavigationItem>
-    )}
+    )} */}
   </ul>
 );
 
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,10 +1,10 @@
 import React from "react";
 import classes from "./Modal.module.css";
-const Modal = props => (
+const Modal = (props) => (
   <div
     style={{
       transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-      opacity: props.show ? "1" : "0"
+      opacity: props.show ? "1" : "0",
     }}
     className={classes.Modal}
   >
